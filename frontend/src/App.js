2cf583faaf2d359b10e3eda6cf0814c23c5fc16a
abhijeet babar <vabhi.babar@gmail.com>
2021-02-26 04:18:32 -0700

    Added navigation buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-
+import { Link, NavLink } from "react-router-dom";
 import logo from "./logo.png";
 import CalViewComponent from "./components/cal-view";
 import { Route, Switch } from "react-router-dom";
@@ -10,8 +10,22 @@ function App() {
   return (
     <div className="App">
       <header>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Gecko Portal</h1>
+        <div style={{ flex: "1" }}>
+          <Link to="/" className="removeAnchorStyles">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Gecko Portal</h1>
+          </Link>
+        </div>
+        <div className="flex-container">
+          <div style={Object.assign({}, { flex: "1" }, {})}>
+            <NavLink to="/" exact style={{ marginRight: "10px" }}>
+              <button>Calendar</button>
+            </NavLink>
+            <NavLink to="/graph">
+              <button>Graph</button>
+            </NavLink>
+          </div>
+        </div>
       </header>
       <Switch>
         <Route path="/" exact component={CalViewComponent}></Route>
